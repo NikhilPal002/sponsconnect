54a4f4c600a9735f85aed5e980b4a395f95d7734
Nikhil <nikhilpal2017@gmail.com>
2023-03-23 17:37:49 +0530

    Created Navbar and Home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="conatiner my-3">
+       
+      </div>
+    </div>
+  )
+}
+
+export default Home
